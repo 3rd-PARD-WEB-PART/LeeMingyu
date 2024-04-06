1df231c743a3b6a25b3dbdea69358e431ede21ba
Mingyu Lee <livingdavid@naver.com>
2024-04-06 15:53:17 +0900

    Seminar 3 Uploaded

---
+++ pard_3rd_seminar_3/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/about';
+import Profile from './Pages/Profile';
+import Articles from './Pages/Articles';
+import Article from './Pages/Article';
+import Layout from './Pages/Layout';
+
+function App() {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} />
+        <Route path='/articles' element={<Articles />}>
+          <Route path=':id' element={<Article />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;

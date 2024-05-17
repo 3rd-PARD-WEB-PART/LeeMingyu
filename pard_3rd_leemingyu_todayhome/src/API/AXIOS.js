7da5e77bde6ab533c07f05430368293af2c62452
Mingyu Lee <livingdavid@naver.com>
2024-05-17 13:14:13 +0900

    AXIOS

---
+++ pard_3rd_leemingyu_todayhome/src/API/AXIOS.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+export const getUserData = async (id) => {
+  try {
+    const response = await axios.get(`http://localhost:3001/users/${id}`);
+    return response.data;
+  } catch(error) {
+    console.error(error);
+  }
+};
+
+export const patchUserData = async(data, id) => {
+  try {
+    const response = await axios.patch(`http://localhost:3001/users/${id}`, data);
+    return response.data;
+  } catch(error) {
+    console.error(error);
+  }
+};
